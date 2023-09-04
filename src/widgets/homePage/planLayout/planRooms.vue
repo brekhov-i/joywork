d10dfba504dc69e4a27e88d02b01bda26b2b019e
brekhov-i <brekhov-i@yandex.ru>
2023-09-04 17:27:45 +0300

    Дописано окно загрузки планировок

--- src/widgets/homePage/planLayout/planRooms.vue
+++ src/widgets/homePage/planLayout/planRooms.vue
@@ -4,8 +4,10 @@
       v-model:visible="isVisibleDeleteAll"
       modal
       header="Вы уверены, что хотите удалить планировки?"
-      :style="{ width: '50vw' }"
       :pt="{
+        root: {
+          class: ['w-[50vw]'],
+        },
         content: {
           class: ['!p-0'],
         },
@@ -48,10 +50,9 @@
       @update:visible="isVisibleOffSide = false"
       modal
       header="Успешно отключено"
-      :style="{ width: '50vw' }"
       :pt="{
         root: {
-          class: ['p-[50px] bg-white'],
+          class: ['p-[50px] bg-white  w-[800px]'],
         },
         content: {
           class: ['!p-0 !border-t !border-grey-400 !pt-5'],
@@ -74,6 +75,61 @@
         <div class="btns w-full flex justify-between">
           <Button severity="info" @click="isVisibleOffSide = false">
             Ок
+          </Button>
+        </div>
+      </template>
+    </Dialog>
+    <Dialog
+      v-model:visible="isVisibleUploadPlan"
+      @update:visible="isVisibleUploadPlan = false"
+      modal
+      header="Загрузка планировок Planoplan"
+      :pt="{
+        root: {
+          class: ['p-[50px] bg-white w-[800px]'],
+        },
+        content: {
+          class: ['!p-0 !border-t !border-grey-400 !pt-5'],
+        },
+        header: {
+          class: ['!p-0 !pb-7.5'],
+        },
+        headerTitle: {
+          class: ['!font-normal'],
+        },
+        footer: {
+          class: ['!p-0 !pt-5'],
+        },
+      }"
+    >
+      <template #default>
+        <p class="text mb-5 text-sm">
+          Введите списком URL планировок. Каждая планировка должна начинаться с
+          новой строки. Для загрузки планировок из Planoplan обязательно
+          опубликуйте у каждой планировки в Planoplan вид "План" или "Вид
+          сверху".
+        </p>
+        <Textarea v-model="urls" class="w-full" rows="5" autoResize />
+      </template>
+      <template #footer>
+        <div class="btns w-full flex justify-between">
+          <Button severity="success">
+            <svg
+              width="18"
+              height="18"
+              viewBox="0 0 18 18"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M15.5325 6.9675L11.0325 2.4675C10.9634 2.40004 10.8819 2.34656 10.7925 2.31C10.6995 2.27221 10.6004 2.25187 10.5 2.25H4.5C3.90326 2.25 3.33097 2.48705 2.90901 2.90901C2.48705 3.33097 2.25 3.90326 2.25 4.5V13.5C2.25 14.0967 2.48705 14.669 2.90901 15.091C3.33097 15.5129 3.90326 15.75 4.5 15.75H13.5C14.0967 15.75 14.669 15.5129 15.091 15.091C15.5129 14.669 15.75 14.0967 15.75 13.5V7.5C15.7506 7.4013 15.7317 7.30345 15.6943 7.21207C15.657 7.12069 15.602 7.03758 15.5325 6.9675ZM6.75 3.75H9.75V5.25H6.75V3.75ZM11.25 14.25H6.75V12C6.75 11.8011 6.82902 11.6103 6.96967 11.4697C7.11032 11.329 7.30109 11.25 7.5 11.25H10.5C10.6989 11.25 10.8897 11.329 11.0303 11.4697C11.171 11.6103 11.25 11.8011 11.25 12V14.25ZM14.25 13.5C14.25 13.6989 14.171 13.8897 14.0303 14.0303C13.8897 14.171 13.6989 14.25 13.5 14.25H12.75V12C12.75 11.4033 12.5129 10.831 12.091 10.409C11.669 9.98705 11.0967 9.75 10.5 9.75H7.5C6.90326 9.75 6.33097 9.98705 5.90901 10.409C5.48705 10.831 5.25 11.4033 5.25 12V14.25H4.5C4.30109 14.25 4.11032 14.171 3.96967 14.0303C3.82902 13.8897 3.75 13.6989 3.75 13.5V4.5C3.75 4.30109 3.82902 4.11032 3.96967 3.96967C4.11032 3.82902 4.30109 3.75 4.5 3.75H5.25V6C5.25 6.19891 5.32902 6.38968 5.46967 6.53033C5.61032 6.67098 5.80109 6.75 6 6.75H10.5C10.6989 6.75 10.8897 6.67098 11.0303 6.53033C11.171 6.38968 11.25 6.19891 11.25 6V4.8075L14.25 7.8075V13.5Z"
+                fill="white"
+              />
+            </svg>
+            Сохранить
+          </Button>
+          <Button severity="secondary" @click="isVisibleUploadPlan = false">
+            Отклонить
           </Button>
         </div>
       </template>
@@ -119,7 +175,9 @@
         </svg>
         Выбрать планировки помещений
       </MyButton>
-      <MyButton :theme="'green'"> Загрузить 3D-планировки </MyButton>
+      <MyButton :theme="'green'" @click="isVisibleUploadPlan = true">
+        Загрузить 3D-планировки
+      </MyButton>
       <Button
         type="button"
         @click="toggle"
@@ -171,6 +229,8 @@ import { PTOptions } from "primevue/ts-helpers";
 
 const isVisibleDeleteAll = ref<boolean>(false);
 const isVisibleOffSide = ref<boolean>(false);
+const isVisibleUploadPlan = ref<boolean>(false);
+const urls = ref<string>("");
 const btnActionStyle = ref<PTOptions<ButtonPassThroughOptions>>({
   root: {
     class: ["!bg-white !border !border-blue !w-max !py-2 !px-3 !text-blue"],
